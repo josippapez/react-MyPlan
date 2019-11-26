60bbb4cdb1725c714c49614c91b50f90ef6d7455
josippapez <papezjosip@gmail.com>
2019-11-26 21:59:36 +0100

    added Project components and added them to Dashboard and App

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function ProjectDetails() {
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card-content">
+                    <span className="card-title">Project Title</span>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, rem. Ipsa dignissimos aliquam non sapiente maiores facilis ducimus dolorum tempore, accusantium corrupti! In veniam voluptas eaque alias voluptates non modi.</p>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                    <div>Posted by Josip Papež</div>
+                    <div>1st September, 12am</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails
