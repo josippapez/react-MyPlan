1a7fcf394e4f90e3f0e4c617b76cf1cc32e41073
josippapez <papezjosip@gmail.com>
2019-11-27 17:15:01 +0100

    passed state as props to components from projectReducer

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function ProjectSummary(){
+function ProjectSummary({project}){
     return(
         <div className="card z-depth-0 project-summary">
             <div className="card-content grey-text text-darken-3">
-                <span className="card-title">Project Title</span>
+                <span className="card-title">{project.title}</span>
                 <p>Posted by Josip Papež</p>
                 <p className="grey-text">1st September, 12Am</p>
             </div>
