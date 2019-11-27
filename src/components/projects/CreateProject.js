e80d710a77976965b859d1acda2c7c7ea32acc22
josippapez <papezjosip@gmail.com>
2019-11-27 17:46:20 +0100

    added create project action

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {createProject} from '../../store/actions/projectActions';
 class CreateProject extends Component {
     state={
         title:'',
@@ -12,7 +13,7 @@ class CreateProject extends Component {
     }
     handleSubmit=(e)=>{
         e.preventDefault();
-        console.log(this.state);
+        this.props.createProject(this.state);
     }
     render() {
         return (
@@ -35,5 +36,9 @@ class CreateProject extends Component {
         )
     }
 }
-
-export default CreateProject;
+const mapDispatchToProps=(dispatch)=>{
+    return {
+        createProject: (project)=>dispatch(createProject(project))
+    }
+}
+export default connect(null,mapDispatchToProps)(CreateProject);
