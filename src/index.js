f48f69bed2346ad4da0bb6d29976cebfb9604f7a
Josip Papez <papezjosip@gmail.com>
2019-11-28 19:57:28 +0100

    downgraded react-redux-firebase to 2.2.6 -fixes firebase

--- src/index.js
+++ src/index.js
@@ -3,21 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {createStore,applyMiddleware,compose} from 'redux';
+import {createStore,applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import rootReducer from './store/reducers/rootReducer';
 import thunk from 'redux-thunk';
-import {getFirestore,reduxFirestore} from 'redux-firestore';
-import {getFirebase, reactReduxFirebase} from 'react-redux-firebase';
-import fbConfig from './config/fbConfig';
 
-const store = createStore(rootReducer,
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
+
+
+const store = createStore(
+    rootReducer,
     compose(
-        applyMiddleware(thunk.withExtraArgument({getFirebase,getFirestore})),
-        reduxFirestore(fbConfig),
-        reactReduxFirebase(fbConfig)
+      applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+      reduxFirestore(fbConfig),
+      reactReduxFirebase(fbConfig)
     )
-);
+  );
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
