699f3ca6f5b17783c10563af55705a13e5fa6e9d
Josip Papez <papezjosip@gmail.com>
2019-11-30 21:25:03 +0100

    date and time of posted projects now showing correctly

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import moment from 'moment';
 
 function ProjectSummary({project}){
     return(
@@ -6,7 +7,7 @@ function ProjectSummary({project}){
             <div className="card-content grey-text text-darken-3">
                 <span className="card-title">{project.title}</span>
                 <p>Posted by {project.authorFirstName} {project.authorLastName}</p>
-                <p className="grey-text">1st September, 12Am</p>
+                <p className="grey-text">{moment(project.createdAt.toDate()).calendar()}</p>
             </div>
         </div>
     )
