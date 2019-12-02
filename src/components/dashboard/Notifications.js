91cacfbd02b43edb45b5125a3e2a21b9524bfc2f
josippapez <papezjosip@gmail.com>
2019-12-02 17:26:05 +0100

    minor css change and connected notifications to firebase

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import moment from 'moment';
 
 function Notifications(props){
+    const {notifications}= props;
     return(
         <div className="section">
             <div className="card z-depth-0">
                 <div className="card-content">
                     <span className="card-title">Notifications</span>
                     <ul className="notifications">
-                        <li>Notification</li>
-                        <li>Notification</li>
-                        <li>Notification</li>
+                        {notifications && notifications.map(notification=>{
+                            return(
+                                <li key={notification.id}>
+                                    <span className="orange-text">{notification.user} </span>
+                                    <span>{notification.content}</span>
+                                    <div className="grey-text notification-date">
+                                        {moment(notification.time.toDate()).fromNow()}
+                                    </div>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
             </div>
